5964e1103a2cd0cff37d2108727799701419beb6
Pruebas13 <jesusrelacionescomerciales6@gmail.com>
2020-04-23 14:36:47 -0400

    Actual

--- sendbymail.php
+++ sendbymail.php
@@ -1,17 +1,17 @@
+
 <?php
     if(isset($_POST['boton'])){
-        if($_POST['nombre'] == ''){
+        if($_POST['name'] == ''){
             $errors[1] = '<span class="error">Ingrese su nombre</span>';
         }else if($_POST['email'] == '' or !preg_match("/^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/",$_POST['email'])){
             $errors[2] = '<span class="error">Ingrese un email correcto</span>';
         }else if($_POST['text'] == ''){
             $errors[3] = '<span class="error">Ingrese un mensaje</span>';
-        
         }else{
             $dest = "jesuszerpaeconomia@gmail.com"; //Email de destino
-            $nombre = $_POST['nombre'];
+            $nombre = $_POST['name'];
             $email = $_POST['email'];
-            $cuerpo = $_POST['text']; //Cuerpo del mensaje
+            $asunto = $_POST['text']; //Cuerpo del mensaje
             //Cabeceras del correo
             $headers = "From: $nombre <$email>\r\n"; //Quien envia?
             $headers .= "X-Mailer: PHP5\n";
@@ -21,7 +21,7 @@
             if(mail($dest,$asunto,$cuerpo,$headers)){
                 $result = '<div class="result_ok">Email enviado correctamente </div>';
                 // si el envio fue exitoso reseteamos lo que el usuario escribio:
-                $_POST['nombre'] = '';
+                $_POST['name'] = '';
                 $_POST['email'] = '';
                 $_POST['text'] = '';
             }else{
